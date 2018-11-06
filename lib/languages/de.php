2bf8e8c50b45409a1d648d4d20bd3e6e4b450a11
jschopplich <27850750+jschopplich@users.noreply.github.com>
2018-11-06 18:20:28 +0100

    Add German translation + English clarification

---
+++ lib/languages/de.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'palette.new.palette'   => 'Neue Palette',
+	'palette.empty.options' => 'Bitte geben Sie Farben in den Feldoptionen an.',
+	'palette.empty.palette' => 'Die Farbpalette ist leer. Bitte wählen Sie ein Bild aus.',
+);
--- lib/languages/en.php
+++ lib/languages/en.php
@@ -2,6 +2,6 @@
 
 return array(
 	'palette.new.palette'   => 'New palette',
-	'palette.empty.options' => 'Please specify colors in the options config',
+	'palette.empty.options' => 'Please specify colors in the field options.',
 	'palette.empty.palette' => 'The color palette is empty. Please select an image.',
 );
