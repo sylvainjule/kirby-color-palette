16ce2002df5a3d95dc085d666510c35e2f5e8ff2
Sylvain Julé <sylvain.jule@free.fr>
2019-03-17 17:26:44 +0100

    Follow the new Pluginkit guidelines

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+if (!class_exists('SylvainJule\ColorPalette')) {
+	require_once __DIR__ . '/lib/color-palette.php';
+}
+
+Kirby::plugin('sylvainjule/color-palette', array(
+	'options' => array(
+		'cache' => true,
+	),
+	'fields' => require_once __DIR__ . '/lib/fields.php',
+	'api'    => require_once __DIR__ . '/lib/api.php',
+    'translations' => array(
+        'en' => require_once __DIR__ . '/lib/languages/en.php',
+        'de' => require_once __DIR__ . '/lib/languages/de.php',
+        'fr' => require_once __DIR__ . '/lib/languages/fr.php',
+    ),
+));
