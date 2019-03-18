6f3af17eeb003310b7c1b048e7f73267c6100373
Sylvain Julé <sylvain.jule@free.fr>
2019-03-18 15:08:45 +0100

    Add missing german translation of 'palette.empty.template'

--- lib/languages/de.php
+++ lib/languages/de.php
@@ -1,7 +1,8 @@
 <?php
 
 return array(
-	'palette.new.palette'   => 'Neue Palette',
-	'palette.empty.options' => 'Bitte geben Sie Farben in den Feldoptionen an.',
-	'palette.empty.palette' => 'Die Farbpalette ist leer. Bitte wählen Sie ein Bild aus.',
+	'palette.new.palette'    => 'Neue Palette',
+	'palette.empty.options'  => 'Bitte geben Sie Farben in den Feldoptionen an.',
+	'palette.empty.template' => 'Es gibt derzeit kein Bild, aus denen Farben extrahiert werden können.',
+	'palette.empty.palette'  => 'Die Farbpalette ist leer. Bitte wählen Sie ein Bild aus.',
 );
